ee508fedb2945a6d9332213fef83d0e6dcf994c1
israel <israel.bejarano.a@gmail.com>
2019-04-07 12:24:30 +0200

    configuracion de charts lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
 
+import { ChartsModule } from 'ng2-charts';
+
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule.forRoot(config),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
